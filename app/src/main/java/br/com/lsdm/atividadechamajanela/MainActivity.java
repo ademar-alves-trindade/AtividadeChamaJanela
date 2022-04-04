17b99e7bbcab4ca7a257a95e316a519e641432c6
trindade <ademar.alves@unemat.br>
2022-04-04 03:24:58 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/lsdm/atividadechamajanela/MainActivity.java
@@ -0,0 +1,21 @@
+package br.com.lsdm.atividadechamajanela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+        Intent i = new Intent(MainActivity.this, Main2Activity.class);
+        MainActivity.this.startActivity(i);
+    }
+}
